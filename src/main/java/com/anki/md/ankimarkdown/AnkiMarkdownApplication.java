ee14a8aab330d33b8e942945184bbcd9d5196497
stanislav.vain <stanislav.vain@gmail.com>
2021-11-06 22:49:34 +0100

    test with local file

--- src/main/java/com/anki/md/ankimarkdown/AnkiMarkdownApplication.java
+++ src/main/java/com/anki/md/ankimarkdown/AnkiMarkdownApplication.java
@@ -7,6 +7,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @Slf4j
 @SpringBootApplication
@@ -17,10 +26,26 @@ public class AnkiMarkdownApplication implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) {
+    public void run(String... args) throws IOException {
+        File file = new ClassPathResource("data/md/german_driving_test.md").getFile();
         Parser parser = Parser.builder().build();
-        Node document = parser.parse("This is *Sparta*");
+        Node document = parser.parse(new String(Files.readAllBytes(file.toPath())));
         HtmlRenderer renderer = HtmlRenderer.builder().build();
-        renderer.render(document);  // "<p>This is <em>Sparta</em></p>\n"
+        String html = renderer.render(document);
+
+        final String[] split = html.split("<hr />");
+        final String collect = Arrays
+                .stream(split)
+                .map(item -> item.replaceAll("[\\n\\r]+", "\t"))
+                .collect(Collectors.joining("\n"));
+
+        log.info(collect);
+
+        File fileout = new File("src/main/resources/data/notesplanetext/filename.txt");
+        FileWriter fw = new FileWriter(fileout.getAbsoluteFile());
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write(collect);
+        bw.close();
+
     }
 }
---
+++ src/main/java/com/anki/md/ankimarkdown/service/NotesPlaneTextService.java
@@ -0,0 +1,7 @@
+package com.anki.md.ankimarkdown.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotesPlaneTextService {
+}
