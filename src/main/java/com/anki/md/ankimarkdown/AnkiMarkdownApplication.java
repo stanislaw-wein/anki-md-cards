39dafdd34bd556edbd5b0a4c98b65b1bef39fef7
stanislaw-wein <stanislav.vain@gmail.com>
2022-04-15 14:08:52 +0200

    update log messages

--- src/main/java/com/anki/md/ankimarkdown/AnkiMarkdownApplication.java
+++ src/main/java/com/anki/md/ankimarkdown/AnkiMarkdownApplication.java
@@ -7,16 +7,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 @AllArgsConstructor
 @SpringBootApplication
 public class AnkiMarkdownApplication implements CommandLineRunner {
-
+    private static final String FILE_DESTINATION = "src/main/resources/data/notesplanetext/%s.txt";
     private final MarkdownService markdownService;
 
     public static void main(String[] args) {
@@ -27,12 +25,26 @@ public class AnkiMarkdownApplication implements CommandLineRunner {
     public void run(String... args) throws IOException {
         final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         log.info("\n >>> Please enter the markdown file path");
+
         final String markdownFilePath = reader.readLine();
-        final File file = new File(markdownFilePath);
-        log.info("\n >>> The file [{}] will be converted to a Anki Plain Test", file.getName());
+        final File markdownFile = new File(markdownFilePath);
+        log.info("\n >>> The file [{}] will be converted to a Anki Plain Test", markdownFile.getName());
 
-        final String notesPlaneText = markdownService.convertMarkdownToNotesPlaneText(file);
-        MarkdownService.createNotesPlaneTextFile(file.getName(), notesPlaneText);
+        final String notesPlaneText = markdownService.convertMarkdownToNotesPlaneText(markdownFile);
+        log.info("\n >>> Conversion completed; Saving the result to a file");
 
+        createNotesPlaneTextFile(markdownFile.getName(), notesPlaneText);
+        log.info("\n >>> Execution finished");
+    }
+
+    private static void createNotesPlaneTextFile(final String fileName, final String content) throws IOException {
+        final File fileOut = new File(String.format(FILE_DESTINATION, fileName));
+        final FileWriter fw = new FileWriter(fileOut.getAbsoluteFile(), StandardCharsets.UTF_8);
+        try (BufferedWriter bw = new BufferedWriter(fw)) {
+            bw.write(content);
+            log.info("\n >>> File location - {}", fileOut.getAbsolutePath());
+        } catch (Exception ex) {
+            log.error("\n >>> Error during file creation {}", ex.getMessage(), ex);
+        }
     }
 }
--- src/main/java/com/anki/md/ankimarkdown/service/MarkdownService.java
+++ src/main/java/com/anki/md/ankimarkdown/service/MarkdownService.java
@@ -10,11 +10,8 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.stream.Collectors;
@@ -32,7 +29,6 @@ public class MarkdownService {
     public static final String FIELD_SEPARATOR = "\t";
     public static final String MARKDOWN_LINE_BREAKERS = "[\\n\\r]+";
     public static final String EMPTY_LINES = "(?m)^\\s*\\r?\\n|\\r?\\n\\s*(?!.*\\r?\\n)";
-    public static final String FILE_DESTINATION = "src/main/resources/data/notesplanetext/%s.txt";
     public static final int FIRST_CARD = 0;
 
     private final Parser markdownParser;
@@ -41,46 +37,46 @@ public class MarkdownService {
     public String convertMarkdownToNotesPlaneText(final File markdownFile) throws IOException {
         final Node markdownDocument = markdownParser.parse(new String(Files.readAllBytes(markdownFile.toPath())));
         final String htmlBlockquoteCards = htmlRenderer.render(markdownDocument);
-        final String[] blockquoteCardsSplit = htmlBlockquoteCards.split(HTML_CARD_SEPARATOR);
-        validateHtmlCards(blockquoteCardsSplit);
+        final String[] htmlCardsArray = htmlBlockquoteCards.split(HTML_CARD_SEPARATOR);
+        validateHtmlCards(htmlCardsArray);
 
-        return Arrays.stream(blockquoteCardsSplit)
+        return Arrays
+                .stream(htmlCardsArray)
                 .map(MarkdownService::convertBlockquotesToPlainTextCard)
                 .collect(Collectors.joining(CARD_SEPARATOR));
-    }
-
-    public static void createNotesPlaneTextFile(final String fileName, final String content) throws IOException {
-        final File fileout = new File(String.format(FILE_DESTINATION, fileName));
-        final FileWriter fw = new FileWriter(fileout.getAbsoluteFile(), StandardCharsets.UTF_8);
-        try (BufferedWriter bw = new BufferedWriter(fw)) {
-            bw.write(content);
-        }
     }
 
     private static String convertBlockquotesToPlainTextCard(String item) {
         final Document doc = Jsoup.parseBodyFragment(item);
         final Elements ankiFields = doc.select(HTML_FIELD_SELECTOR);
-        return ankiFields.stream()
-                .map(field -> field.html().replaceAll(EMPTY_LINES, "").replaceAll(MARKDOWN_LINE_BREAKERS, ""))
+        return ankiFields
+                .stream()
+                .map(field -> field
+                        .html()
+                        .replaceAll(EMPTY_LINES, "")
+                        .replaceAll(MARKDOWN_LINE_BREAKERS, ""))
                 .collect(Collectors.joining(FIELD_SEPARATOR));
     }
 
     private static void validateHtmlCards(final String[] htmlCards) {
         if (htmlCards == null || htmlCards.length == 0) {
-            log.error("No cards detected");
+            log.error("\n >> No cards detected");
             throw new RuntimeException("No cards detected");
         }
         final int numberOfFields = getNumberOfFields(htmlCards[FIRST_CARD]);
-        log.info("Number of cards is {}; Number of fields in the card is: {}", htmlCards.length, numberOfFields);
-        Arrays.asList(htmlCards).forEach(item -> {
-            if (getNumberOfFields(item) != numberOfFields) {
-                log.warn("This card has an incorrect number of fields: {}", item);
-            }
-        });
+        log.info("\n >> Number of cards is {}; Number of fields in the card is: {}", htmlCards.length, numberOfFields);
+        Arrays.asList(htmlCards)
+              .forEach(item -> {
+                  if (getNumberOfFields(item) != numberOfFields) {
+                      log.warn("This card has an incorrect number of fields: {}", item);
+                  }
+              });
     }
 
     private static int getNumberOfFields(String htmlCard) {
-        return Jsoup.parseBodyFragment(htmlCard).getElementsByTag(HTML_FIELD_SELECTOR).size();
+        return Jsoup.parseBodyFragment(htmlCard)
+                    .getElementsByTag(HTML_FIELD_SELECTOR)
+                    .size();
     }
 
 }
